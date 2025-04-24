229d65a1185daf3806aedae01f8590f44ead5efe
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-04-24 22:38:36 +0100

    Materials : unique + lower bound and upper bound

---
+++ Level 1/STL/stl_vector.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void	Materials()
+{
+	
+
+
+
+	/*    *********   Unique *******     */
+
+
+	//vector<int> arr = {10, 2 , 4, 10, 5 , 42 , 42 , 13, 4, 2};
+
+    // Sort the vector
+    // sort(arr.begin(), arr.end());
+
+    // Remove duplicates
+	//
+	// Unique : does NOT remove elements from the vector
+	//
+	// Moves all unique elements to the beginning of the container
+	//
+	//Returns an iterator (last) to the new logical end of the unique part.
+	//
+
+    // auto last = unique(arr.begin(), arr.end());
+    // arr.erase(last, arr.end()); // remove duplicate elements
+
+    // // Print the result
+    // for (int i = 0; i < arr.size(); i++)
+   	// {
+    //     cout << arr[i] << endl;
+    // }
+
+
+
+	/*  ********* lower bound  && upper bound   *********/
+
+
+	// lower_bound(start, end, value)
+	// Finds the first position where the value can be inserted without breaking order >= value)
+	// value = 2 && 2 is not found  --> give : 3 
+
+	// upper_bound(start, end, value)
+	// Finds the first position where value is strictly greater  > value
+
+
+	vector<int> v = {1, 3, 4, 5, 7, 8};
+
+    sort(v.begin(), v.end()); // Must be sorted 
+
+	// *** iteration 
+
+	//vector <int>::iterator it  = lower_bound(v.begin(), v.end(), 3);
+
+	// you can use like this :
+
+	//auto iter = lower_bound(v.begin(), v.end(), 2);
+
+	//cout << "simple iter : " << *it << "\t auto iter : " << *iter << endl;
+
+
+	// auto it = upper_bound(v.begin(), v.end(), 7);
+
+	// auto i = lower_bound(v.begin(), v.end(), 6);
+
+	// ** trick : 
+
+	auto it = upper_bound(v.begin(), v.end(), 7 - 1); // like using lower_bound is simpl triic
+
+	auto i = lower_bound(v.begin(), v.end(), 7);
+
+	cout << "upper bound " << *it << "\nlower bound is : " << *i << endl; 
+
+
+
+
+}
+
+
+
+
+
+int main(void)
+{
+	Materials();
+
+	return 0;
+}
